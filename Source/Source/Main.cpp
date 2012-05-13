fb40109879c4a79172aca49cd765bbe224478de0
Rico <devnull@localhost>
2012-05-13 22:09:33 +0000

    Initial commit. Makefile has been tested for making device debug builds.
    /TODO/
    Need to write a README for make instructions and how to extend the build

---
+++ Source/Source/Main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <stdlib.h>
+#include <stdio.h>
+
+int main( int p_Argc, char **p_ppArgv )
+{
+	std::cout << "std::cout: Hello, PlayBook!" << std::endl;
+	fprintf( stderr, "printf: Hello, PlayBook!\n" );
+
+	fflush( stdout );
+	return 0;
+}
+
